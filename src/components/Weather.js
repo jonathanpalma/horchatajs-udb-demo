c086cbc039841a01ff1e3e781123178882512f02
Jonathan Palma <tanpalma04@gmail.com>
2018-03-10 03:04:33 -0600

    Added input text to handle city state

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -5,10 +5,27 @@ const API_KEY = '6a78596d062df78380eff5944c4e5567';
 const ROOT_URL = 'http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}';
 
 export default class Weather extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { city: '' };
+
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({ city: event.target.value });
+  }
+
   render() {
     return (
       <div>
         I'm the Weather component
+        <form>
+          <input type="text" name="city" onChange={this.onInputChange} value={this.state.city} />
+          <button>Submit</button>
+        </form>
+
       </div>
     )
   }
