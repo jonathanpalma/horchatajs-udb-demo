d3c9a71b37516296d6782381a8e9f8f673091128
Jonathan Palma <tanpalma04@gmail.com>
2018-03-10 03:15:44 -0600

    Added button to handle the form submition and implement fecth request using axios to get the data from the ws

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 const API_KEY = '6a78596d062df78380eff5944c4e5567';
-const ROOT_URL = 'http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}';
+const ROOT_URL = 'http://api.openweathermap.org/data/2.5/forecast?appid=' + API_KEY;
 
 export default class Weather extends Component {
 
@@ -11,19 +11,38 @@ export default class Weather extends Component {
     this.state = { city: '' };
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  fetchWeather(url) {
+    console.log('FETCHING... ', url);
+    axios.get(url)
+      .then((response) => {
+        console.log('Response ', response.data);
+        this.setState({ data: response.data });
+      })
+      .catch((err) => {
+        console.log('An error has occurred', err);
+      })
   }
 
   onInputChange(event) {
     this.setState({ city: event.target.value });
   }
 
+  onFormSubmit(event) {
+    event.preventDefault();
+    const url = ROOT_URL + '&q=' + this.state.city + ',sv';
+    this.fetchWeather(url);
+  }
+
   render() {
     return (
       <div>
         I'm the Weather component
-        <form>
+        <form onSubmit={this.onFormSubmit}>
           <input type="text" name="city" onChange={this.onInputChange} value={this.state.city} />
-          <button>Submit</button>
+          <button type="submit">Submit</button>
         </form>
 
       </div>
